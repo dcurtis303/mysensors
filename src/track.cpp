aa252e6159186189c09943fc1cf64537084609a4
Dave <dcurtis303@gmail.com>
2021-04-29 07:30:09 -0400

    0730

--- src/track.cpp
+++ src/track.cpp
@@ -125,48 +125,6 @@ int Track::writecfg(const char *output_file)
     return (EXIT_SUCCESS);
 }
 
-void enumfeature(void)
-{
-    sensors_chip_name const *cn;
-    int c = 0;
-
-    while ((cn = sensors_get_detected_chips(0, &c)) != 0)
-    {
-        printf("Chip: %s:%s\n", cn->prefix, cn->path);
-
-        sensors_feature const *feat;
-        int f = 0;
-
-        while ((feat = sensors_get_features(cn, &f)) != 0)
-        {
-            printf("%d:%s\n", f, feat->name);
-
-            sensors_subfeature const *subf;
-            int s = 0;
-
-            while ((subf = sensors_get_all_subfeatures(cn, feat, &s)) != 0)
-            {
-                printf("%d:%d:%s:%d = ", f, s, subf->name, subf->number);
-
-                double val;
-                if (subf->flags & SENSORS_MODE_R)
-                {
-                    int rc = sensors_get_value(cn, subf->number, &val);
-                    if (rc < 0)
-                    {
-                        printf("err: %d", rc);
-                    }
-                    else
-                    {
-                        printf("%f", val);
-                    }
-                }
-                printf("\n");
-            }
-        }
-    }
-}
-
 void Track::addtrack(string chip, string subf)
 {
     sensors_chip_name const *cn;
